228eeaf4551de1b027ab163b5f5375b4ccee70b4
Hieu Nguyen <hieuk09@gmail.com>
2019-08-13 02:23:17 +0800

    Add grammar fuzzer

--- lib/fuzzer.rb
+++ lib/fuzzer.rb
@@ -2,7 +2,9 @@ require "fuzzer/version"
 require 'fuzzer/strategies/random'
 require 'fuzzer/strategies/mutational'
 require 'fuzzer/strategies/greybox'
+require 'fuzzer/strategies/grammar'
 require 'fuzzer/runner'
+require 'fuzzer/generator'
 
 module Fuzzer
   class Error < StandardError; end
---
+++ lib/fuzzer/strategies/grammar.rb
@@ -0,0 +1,23 @@
+require 'fuzzer/strategies/base'
+
+module Fuzzer
+  module Strategies
+    class Grammar < Base
+      def initialize(generator, trials: 10)
+        super([])
+        @generator = generator
+        @trials = trials
+      end
+
+      def fuzz(_)
+        trials.times.each do
+          yield generator.generate
+        end
+      end
+
+      private
+
+      attr_reader :generator, :trials
+    end
+  end
+end
